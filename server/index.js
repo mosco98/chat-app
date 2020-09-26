219b0a9172f9b0bdd4b941de4e81555dbdb7ff44
unknown <moscolyno98@gmail.com>
2020-09-26 08:34:49 +0100

    Ready Ready!

--- server/index.js
+++ server/index.js
@@ -1,36 +1,31 @@
+const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
-const http = require('http')
 const cors = require('cors')
 
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users')
 
-const PORT = process.env.PORT || 5000
-
 const router = require('./router')
 
-// App/Server init
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 
-// Socket.io connection
-io.on('connection', (socket) => {
+app.use(cors())
+app.use(router)
+
+io.on('connect', (socket) => {
   socket.on('join', ({ name, room }, callback) => {
     const { error, user } = addUser({ id: socket.id, name, room })
 
     if (error) return callback(error)
 
-    socket.emit('message', { user: 'Admin', text: `${user.name}, welcome to the room ${user.room}` })
-
-    socket.broadcast.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has joined!` })
-
     socket.join(user.room)
 
-    io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) })
+    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.` })
+    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` })
 
-    console.log(user)
-    console.log(getUsersInRoom(user.room))
+    io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) })
 
     callback()
   })
@@ -47,15 +42,10 @@ io.on('connection', (socket) => {
     const user = removeUser(socket.id)
 
     if (user) {
-      io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left` })
+      io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` })
       io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) })
     }
   })
 })
 
-// Middleware
-app.use(cors())
-app.use(router)
-
-// Start server
-server.listen(PORT, () => console.log(`Server has started on port ${PORT}`))
+server.listen(process.env.PORT || 5000, () => console.log(`Server has started.`))
